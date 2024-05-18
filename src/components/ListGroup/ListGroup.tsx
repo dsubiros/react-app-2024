f4730de2760426af7dd54979058113fb6c442609
Dariel Subiros <dsubiros@gmail.com>
2024-05-18 14:12:44 -0400

    Adding styled-components, I don't like it

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,14 +1,29 @@
 import { useState } from "react";
-import styles from  './ListGroup.module.css';
+import styles from "./ListGroup.css";
+import styled from "styled-components";
+
+const List = styled.ul`
+   list-style: none;
+   padding: 10px;
+`;
+
+interface ListItemProps {
+  active: boolean;
+}
+
+const ListItem = styled.li<ListItemProps>`
+   padding: 5px 0;
+   background: ${props => props.active ? 'blue': 'none'}
+`;
 
 export interface Props {
-  items: string[]; 
+  items: string[];
   heading: string;
   onSelectItem: (item: string) => void;
 }
 
 function ListGroup({ items, heading, onSelectItem }: Props) {
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(0);
   const handleClick = (x: number) => {
     setSelectedIndex(x);
     onSelectItem && onSelectItem(items[selectedIndex]);
@@ -18,19 +33,23 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
     <>
       <h1>{heading}</h1>
       {!items.length && <p>No items were found</p>}
-      <ul className={[styles.listGroup, styles.container].join(' ')}>
+      {/* <ul className={[styles.listGroup, styles.container].join(" ")}> */}
+      <List 
+      // className={[styles.listGroup, styles.container].join(" ")}
+      >
         {items.map((x, idx) => (
-          <li
+          <ListItem
+            active={selectedIndex === idx}
             key={idx.toString()}
-            className={
-              "list-group-item " + (selectedIndex === idx ? "active" : "")
-            }
+            // className={
+            //   "list-group-item " + (selectedIndex === idx ? "active" : "")
+            // }
             onClick={() => handleClick(idx)}
           >
             {x}
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
